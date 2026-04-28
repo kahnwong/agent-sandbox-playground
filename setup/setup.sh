f843f6e3bb88dbe3558fe8718489bb51b1fc8606
Karn Wong <karn@karnwong.me>
2026-04-28 14:06:25 +0700

    add go workload

--- setup/setup.sh
+++ setup/setup.sh
@@ -8,3 +8,4 @@ kubectl apply -f "https://github.com/kubernetes-sigs/agent-sandbox/releases/down
 # `sandbox-router` have to be in the same space as `sandbox-template`
 kubectl apply -f sandbox-router/sandbox_router.yaml -n default
 kubectl apply -f python-sandbox-template.yaml -n default
+kubectl apply -f sandbox-template.yaml -n default  # added by me

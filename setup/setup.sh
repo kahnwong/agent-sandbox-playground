a0f256d3a73383923b479cad28be2869c5dc2959
Karn Wong <karn@karnwong.me>
2026-04-28 12:36:41 +0700

    install sandbox-router

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,6 +2,7 @@
 
 # https://github.com/kubernetes-sigs/agent-sandbox/releases
 export VERSION="v0.4.2"
-
 kubectl apply -f "https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/manifest.yaml"
 kubectl apply -f "https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/extensions.yaml"
+
+kubectl apply -f sandbox-router/sandbox_router.yaml -n agent-sandbox-system
